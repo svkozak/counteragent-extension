8fecee6f84dd588e7c9fc7455987fa12b5f5f429
Sergii Kozak <Sergii.Kozak@telus.com>
2022-01-14 12:28:01 -0800

    Update for other foreign agents, new icon added

--- Shared (Extension)/Resources/background.js
+++
@@ -1,5 +0,0 @@
-browser.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-  if (tab.status == 'complete') {
-      browser.tabs.sendMessage(tabId, {"message": "counteragent"});
-  }
-});
